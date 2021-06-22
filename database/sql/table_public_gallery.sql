12dcafc895bdb9c673b6ea3835b4475aa8c35724
Sysborg <59512284+sysborg@users.noreply.github.com>
2021-06-21 22:49:05 -0300

    Update table_public_gallery.sql

--- database/sql/table_public_gallery.sql
+++ database/sql/table_public_gallery.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public.gallery(
 	id_gallery BIGSERIAL NOT NULL CONSTRAINT public_gallery_pkey PRIMARY KEY,
-	gallery_date TIMESTAMPTZ NOT NULL
+	gallery_date TIMESTAMPTZ NOT NULL DEFAULT NOW()
 ) WITH(
 oids=false
 );
